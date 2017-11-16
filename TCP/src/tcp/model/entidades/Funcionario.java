3a857d155f1a878b8504ba84d810a8d8cfe2cf7d
Caio Victor Araujo Campos <cvac@WINDOWS.CIN.UFPE.BR>
2017-11-16 15:45:29 -0300

    asd
    asdasdasd

---
+++ TCP/src/tcp/model/entidades/Funcionario.java
@@ -0,0 +1,36 @@
+package tcp.model.entidades;
+
+public class Funcionario extends ParteInteressada{
+
+
+public Funcionario (String codigo, String dataInicioRelacao, String dataFimRelacao) {
+	
+	this.setCodigo(codigo);
+	this.setDataInicioRelacao(dataInicioRelacao);
+	this.setDataFimRelacao(dataFimRelacao);
+
+}
+
+public String gerarCodigo(ParteInteressada parteInteressada) {
+	return "";
+}
+
+public String gerarDataInicioRelacao(ParteInteressada parteInteressada) {
+	return "";
+}
+
+public String gerarDataFimRelacao(ParteInteressada parteInteressada) {
+	return "";
+}
+
+
+
+
+
+
+
+
+
+
+
+}
